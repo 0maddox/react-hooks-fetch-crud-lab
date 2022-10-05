cf70dfa86924734649c059463d6d5eead14757a2
nicholas <nickkiim7@gmail.com>
2022-10-05 17:03:48 +0300

    completed code challange

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 function QuestionForm(props) {
   const [formData, setFormData] = useState({
     prompt: "",
@@ -9,34 +8,34 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
   function handleChange(event) {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
     });
   }
-
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
-  }
 
-  const newFormData = {
-    "prompt": formData.prompt,
-    "answers": [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
-    "correctIndex": formData.correctIndex
-    };
-
+    //
     fetch("http://localhost:4000/questions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newFormData),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
+  }
 
   return (
     <section>
@@ -104,6 +103,5 @@ function QuestionForm(props) {
       </form>
     </section>
   );
-
-  }
+}
 export default QuestionForm;
